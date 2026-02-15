75feda646a59a4f521187b9541008f7a201735d2
somaye <somayekhaleghi@gmail.com>
2026-02-15 12:54:23 +0330

    set correct action for service intent filter

--- ClientApp/app/src/main/java/com/example/clientapp/MainActivity.kt
+++ ClientApp/app/src/main/java/com/example/clientapp/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.example.clientapp
 
 
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
@@ -16,6 +11,7 @@ import android.os.RemoteException
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.aidlrandomservice.IMyAidlInterface
 
 
